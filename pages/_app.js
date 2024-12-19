94afac95ec5b182af00792354f4123b9186e6167
Nadia <nadiahosisy@gmail.com>
2024-12-19 20:07:17 +0200

    fix css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import Preloader from "@/src/layouts/Preloader";
 import "@/styles/globals.css";
+import '../public/styles/bootstrap.css';
+import '../public/styles/animate.css';
+import '../public/styles/magnific-popup.css';
+import '../public/styles/splitting.css';
+import '../public/styles/swiper.css';
+import '../public/style.css';
 import { Fragment } from "react";
 const App = ({ Component, pageProps }) => {
   return (
